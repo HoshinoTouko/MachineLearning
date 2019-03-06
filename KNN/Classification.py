bbf862dc2b993beaa4cb342f221e80aec30bc070
HoshinoTouko <oranly@hotmail.com>
2019-03-06 21:59:13 +0800

    Add specific dataset.

--- KNN/Classification.py
+++ KNN/Classification.py
@@ -9,10 +9,11 @@
 """
 from collections import Counter
 from sklearn.model_selection import train_test_split
+
+import os
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
-
 import itertools
-
 import data.fake.classification as dt
 
 
@@ -49,13 +50,20 @@ class KNNClassifier:
 
 
 def main():
-    data = dt.generate_two_dimensional_linear_data_for_test(200)
-    features = [x for x in itertools.chain(data[0], data[1])]
-    labels = [-1 for x in range(len(data[0]))]
-    labels += [1 for x in range(len(data[1]))]
+    # data = dt.generate_two_dimensional_linear_data_for_test(200)
+    # features = [x for x in itertools.chain(data[0], data[1])]
+    # labels = [-1 for x in range(len(data[0]))]
+    # labels += [1 for x in range(len(data[1]))]
+    features = []
+    labels = []
+    with open(os.path.abspath('../data/files/classification/1.csv'), 'r') as fi:
+        for line in fi.readlines():
+            _line = line.strip().split(',')
+            features.append(list(map(float, _line[:2])))
+            labels.append(_line[2])
 
     features_train, features_test, labels_train, labels_test = train_test_split(
-        features, labels, test_size=0.25, random_state=50)\
+        features, labels, test_size=0.75, random_state=50)\
 
     _KNNClassifier = KNNClassifier(features_train, labels_train)
 
@@ -66,12 +74,17 @@ def main():
     # Draw plot
     for i in range(len(features_train)):
         color = 'green'
-        if labels_train[i] == 1:
+        if labels_train[i] == 'A':
             color = 'red'
-        plt.scatter(features_train[i][0], features_train[i][1], color=color)
+        plt.scatter(features_train[i][0], features_train[i][1], color=color, label='Marked')
     for i in range(len(features_test)):
         color = 'blue'
-        plt.scatter(features_test[i][0], features_test[i][1], color=color)
+        plt.scatter(features_test[i][0], features_test[i][1], color=color, label='Unmarked data')
+
+    red_patch = mpatches.Patch(color='red', label='Marked A')
+    green_patch = mpatches.Patch(color='green', label='Marked B')
+    blue_patch = mpatches.Patch(color='blue', label='Unmarked data')
+    plt.legend(handles=[red_patch, green_patch, blue_patch])
     plt.show()
 
 
